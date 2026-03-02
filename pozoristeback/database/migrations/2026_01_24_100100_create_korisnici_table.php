cd2601252b1b42dfc8666cce7f23d4ed89debc55
anjaperovic22 <anjaivaperovic@gmail.com>
2026-03-02 18:56:32 +0100

    Izmenjena migracija za kreiranje tabele korisnici zbog seedera

--- pozoristeback/database/migrations/2026_01_24_100100_create_korisnici_table.php
+++ pozoristeback/database/migrations/2026_01_24_100100_create_korisnici_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('uloga', ['ADMIN', 'KORISNIK']);
+            $table->enum('uloga', ['admin', 'klijent']);
             $table->rememberToken();
             $table->timestamps();
         });
