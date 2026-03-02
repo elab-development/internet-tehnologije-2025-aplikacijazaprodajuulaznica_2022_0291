9fb828644900f62c6cc586eb2493be5d660f1f73
anjaperovic22 <anjaivaperovic@gmail.com>
2026-03-02 19:05:00 +0100

    Izmenjena migracija za kreiranje tabele rezervacije zbog seedera

--- pozoristeback/database/migrations/2026_01_24_100500_create_rezervacije_table.php
+++ pozoristeback/database/migrations/2026_01_24_100500_create_rezervacije_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('rezervacije', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')
+            $table->foreignId('korisnik_id')
                 ->constrained('korisnici')
                 ->cascadeOnDelete();
             $table->decimal('ukupna_cena',8,2);
