cce2c68076abfb5ef99b01e9daed44dd95428ecd
anjaperovic22 <anjaivaperovic@gmail.com>
2026-03-08 19:36:23 +0100

    Izmenjen http.js zbog uploada na cloud

--- pozoristefront/src/api/http.js
+++ pozoristefront/src/api/http.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // const BASE_URL = 'http://127.0.0.1:8000/api'; 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = window.location.hostname === 'localhost' 
+    ? 'http://localhost:8000/api' 
+    : 'https://internet-tehnologije-2025-3h3i.onrender.com/api';
 
 const http = axios.create({
     baseURL: BASE_URL,
