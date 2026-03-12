6b050bd1073fcb5c2aa61ba28b82a0aa5d9c445a
anjaperovic22 <anjaivaperovic@gmail.com>
2026-03-12 02:36:54 +0100

    Dodata statistika prodaje

--- pozoristefront/src/pages/LogIn.jsx
+++ pozoristefront/src/pages/LogIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // <--- DODATO: Uvozimo useNavigate
+import { useNavigate } from 'react-router-dom'; 
 import http from '../api/http';
 import '../css/LogIn.css'; 
 
